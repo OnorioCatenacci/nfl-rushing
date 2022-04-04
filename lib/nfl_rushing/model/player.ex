42978b8f160169c6c4eb301f97be83a838c4079f
Onorio Catenacci <catenacci@ieee.org>
2022-04-04 17:28:18 -0400

    Adding ability to filter by name
    I refactored the code to move the code which populates the list to the Player model.
    
    I also added a function to select all unique last names for purposes of allowing filtering by last name.

--- lib/nfl_rushing/model/player.ex
+++ lib/nfl_rushing/model/player.ex
@@ -2,6 +2,7 @@ defmodule NflRushing.Model.Player do
   @moduledoc false
   use Ecto.Schema
   import Ecto.Changeset
+  require Ecto.Query
 
   schema "players" do
     field :first_name, :string
@@ -52,4 +53,48 @@ defmodule NflRushing.Model.Player do
     |> validate_number(:rushes_greater_than_40_yards, greater_than_or_equal_to: 0)
     |> validate_number(:rushing_fumbles, greater_than_or_equal_to: 0)
   end
+
+  def last_name_list() do
+    Ecto.Query.from(
+      pl in NflRushing.Model.Player,
+      select: pl.last_name
+    )
+    |> Ecto.Query.distinct(true)
+  end
+
+  def list_players(sort_order) do
+    Ecto.Query.from(
+      pl in NflRushing.Model.Player,
+      join: t in NflRushing.Model.Team,
+      on: t.id == pl.teams_id,
+      join: po in NflRushing.Model.Position,
+      on: po.id == pl.positions_id,
+      order_by: ^filter_player_by(sort_order),
+      select: [
+        first_name: pl.first_name,
+        last_name: pl.last_name,
+        team: fragment("concat(?,' ',?)", t.city, t.nickname),
+        position: po.description,
+        rushing_attempts_per_game: pl.rushing_attempts_per_game,
+        rushing_attempts: pl.rushing_attempts,
+        total_rushing_yards: pl.total_rushing_yards,
+        average_rushing_yards_per_attempt: pl.average_rushing_yards_per_attempt,
+        average_rushing_yards_per_game: pl.average_rushing_yards_per_game,
+        total_rushing_touchdowns: pl.total_rushing_touchdowns,
+        longest_rush: pl.longest_rush,
+        rushing_first_downs: pl.rushing_first_downs,
+        rushes_greater_than_20_yards: pl.rushes_greater_than_20_yards,
+        rushes_greater_than_40_yards: pl.rushes_greater_than_40_yards,
+        rushing_fumbles: pl.rushing_fumbles
+      ]
+    )
+  end
+
+  defp filter_player_by("total_rushing_yds_asc"), do: [asc: :total_rushing_yards]
+  defp filter_player_by("total_rushing_yds_desc"), do: [desc: :total_rushing_yards]
+  defp filter_player_by("longest_rush_asc"), do: [asc: :longest_rush]
+  defp filter_player_by("longest_rush_desc"), do: [desc: :longest_rush]
+  defp filter_player_by("total_rushing_touchdowns_asc"), do: [asc: :total_rushing_touchdowns]
+  defp filter_player_by("total_rushing_touchdowns_desc"), do: [desc: :total_rushing_touchdowns]
+  defp filter_player_by(_), do: []
 end
--- lib/nfl_rushing_web/controllers/page_controller.ex
+++ lib/nfl_rushing_web/controllers/page_controller.ex
@@ -1,76 +1,39 @@
 defmodule NflRushingWeb.PageController do
   use NflRushingWeb, :controller
   alias NflRushing.Model.Player
-  require Ecto.Query
 
   def index(conn, _params) do
-    player_list = list_players("no_sort") |> NflRushing.Repo.all()
-    render(conn, "index.html", players: player_list)
+    render(conn, "index.html", set_data_for_view("no_sort"))
   end
 
   def total_rushing_yds(conn, _params) do
-    player_list = list_players("total_rushing_yds_asc") |> NflRushing.Repo.all()
-    render(conn, "index.html", players: player_list)
+    render(conn, "index.html", set_data_for_view("total_rushing_yds_asc"))
   end
 
   def total_rushing_yds_desc(conn, _params) do
-    player_list = list_players("total_rushing_yds_desc") |> NflRushing.Repo.all()
-    render(conn, "index.html", players: player_list)
+    player_list = Player.list_players("total_rushing_yds_desc") |> NflRushing.Repo.all()
+    render(conn, "index.html", set_data_for_view("total_rushing_yds_desc"))
   end
 
   def longest_rush(conn, _params) do
-    player_list = list_players("longest_rush_asc") |> NflRushing.Repo.all()
-    render(conn, "index.html", players: player_list)
+    render(conn, "index.html", set_data_for_view("longest_rush_asc"))
   end
 
   def longest_rush_desc(conn, _params) do
-    player_list = list_players("longest_rush_desc") |> NflRushing.Repo.all()
-    render(conn, "index.html", players: player_list)
+    render(conn, "index.html", set_data_for_view("longest_rush_desc"))
   end
 
   def total_rushing_tds(conn, _params) do
-    player_list = list_players("total_rushing_touchdowns_asc") |> NflRushing.Repo.all()
-    render(conn, "index.html", players: player_list)
+    render(conn, "index.html", set_data_for_view("total_rushing_touchdowns_asc"))
   end
 
   def total_rushing_tds_desc(conn, _params) do
-    player_list = list_players("total_rushing_touchdowns_desc") |> NflRushing.Repo.all()
-    render(conn, "index.html", players: player_list)
+    render(conn, "index.html", set_data_for_view("total_rushing_touchdowns_desc"))
   end
 
-  def list_players(sort_order) do
-    Ecto.Query.from(
-      pl in Player,
-      join: t in NflRushing.Model.Team,
-      on: t.id == pl.teams_id,
-      join: po in NflRushing.Model.Position,
-      on: po.id == pl.positions_id,
-      order_by: ^filter_player_by(sort_order),
-      select: [
-        first_name: pl.first_name,
-        last_name: pl.last_name,
-        team: fragment("concat(?,' ',?)", t.city, t.nickname),
-        position: po.description,
-        rushing_attempts_per_game: pl.rushing_attempts_per_game,
-        rushing_attempts: pl.rushing_attempts,
-        total_rushing_yards: pl.total_rushing_yards,
-        average_rushing_yards_per_attempt: pl.average_rushing_yards_per_attempt,
-        average_rushing_yards_per_game: pl.average_rushing_yards_per_game,
-        total_rushing_touchdowns: pl.total_rushing_touchdowns,
-        longest_rush: pl.longest_rush,
-        rushing_first_downs: pl.rushing_first_downs,
-        rushes_greater_than_20_yards: pl.rushes_greater_than_20_yards,
-        rushes_greater_than_40_yards: pl.rushes_greater_than_40_yards,
-        rushing_fumbles: pl.rushing_fumbles
-      ]
-    )
+  defp set_data_for_view(player_sort) do
+    player_list = Player.list_players(player_sort) |> NflRushing.Repo.all()
+    last_name_list = Player.last_name_list() |> NflRushing.Repo.all()
+    [players: player_list, last_name_list: last_name_list]
   end
-
-  defp filter_player_by("total_rushing_yds_asc"), do: [asc: :total_rushing_yards]
-  defp filter_player_by("total_rushing_yds_desc"), do: [desc: :total_rushing_yards]
-  defp filter_player_by("longest_rush_asc"), do: [asc: :longest_rush]
-  defp filter_player_by("longest_rush_desc"), do: [desc: :longest_rush]
-  defp filter_player_by("total_rushing_touchdowns_asc"), do: [asc: :total_rushing_touchdowns]
-  defp filter_player_by("total_rushing_touchdowns_desc"), do: [desc: :total_rushing_touchdowns]
-  defp filter_player_by(_), do: []
 end
